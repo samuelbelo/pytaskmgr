42cebcd28676f1f26434f1a57c479bec68ecca2a
Samuel Belo <samuel.belo@al.infnet.edu.br>
2019-08-27 13:01:44 -0300

    adicionado contador, fundo preto na página

--- PyTaskMgn.py
+++ PyTaskMgn.py
@@ -2,19 +2,19 @@ import pygame
 import random
 import psutil
 
-branco = (255,255,255)
-preto = (0,0,0)
+branco = (255, 255, 255)
+preto = (0, 0, 0)
+azul = (0, 0, 255)
+vermelho = (255, 0, 0)
 
 largura_tela, altura_tela = 800,600
 tela = pygame.display.set_mode((largura_tela, altura_tela))
 pygame.display.set_caption("Gerenciador de tarefas")
-pygame.font.init()
 terminou = False
 
 
 pygame.font.init()
 font = pygame.font.Font(None, 32)
-
 
 def mostra_uso_memoria():
     mem = psutil.virtual_memory()
@@ -28,13 +28,18 @@ def mostra_uso_memoria():
     text = font.render(texto_barra, 1, branco)
     tela.blit(text, (20, 10))
 
+cont = 60
 
+clock = pygame.time.Clock()
 while not terminou:
-    tela.fill(branco)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminou = True
+        if cont == 60:
+            mostra_uso_memoria()
+            cont = 0
     pygame.display.update()
-    clock.tick(1)
+    clock.tick(60)
+    cont = cont + 1
 
 pygame.display.quit()
