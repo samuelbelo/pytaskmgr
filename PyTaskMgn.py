1981fa5c8764bb6ec6e4bd11538e9e8398679a06
Samuel Belo <samuel.belo@al.infnet.edu.br>
2019-08-27 11:29:02 -0300

    criação da função mostrar_memoria, bug integer para flot

--- PyTaskMgn.py
+++ PyTaskMgn.py
@@ -19,9 +19,20 @@ def mostra_titulo():
     tela.blit(text, textpos)
 
 
+def mostra_memoria():
+    mem = psutil.virtual_memory()
+    capacidade = round(mem.total / (1024 * 1024 * 1024), 2)
+    fonte_memo = pygame.font.Font(None, 24)
+    """estou recebendo o erro: TypeError: integer argument expected, got float na linha abaixo"""
+    text = fonte_memo.render("Memoria total: ", capacidade, 2, preto)
+    textpos = fonteMemo.get_rect(centerx=tela.get_width() / 2)
+    tela.blit(text,textpos)
+
+
 while not terminou:
     tela.fill(branco)
     mostra_titulo()
+    mostra_memoria()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminou = True
