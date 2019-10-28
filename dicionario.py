eaa0d8db94b950f6019491ce5760b9e632d5a481
Samuel Belo <samuel.belo@al.infnet.edu.br>
2019-10-28 06:52:13 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,13 @@
+import psutil
+
+lista_dicionarios = []
+
+for p in psutil.process_iter():
+    lista_dicionarios.append(
+        {"Pid": p.pid,
+         "Nome": p.name,
+         "RSS": p.memory_info().rss,
+         "VMS": p.memory_info().vms,
+         "Status": p.status()})
+
+print(lista_dicionarios)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
-import psutil
-import time
 import os
+import time
+# import psutil
+from dicionario import *
 from cores import *
-from teste import *
+
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'                                  #inicializa a tela no centro display
 titulo_abas = ['Arquivos', 'Processos', 'Rede 1', 'Rede 2']
@@ -22,6 +23,7 @@ clock = pygame.time.Clock()
 def mostra_tempo(tempo, minuto):
     pygame.draw.rect(tela, cinza, (0, 0, 800, 50), 0)
     mostra_titulo_tela()
+    desenha_abas()
     fonte = pygame.font.Font(None, 24)
     text = fonte.render('Tempo: ' + f'{minuto}' + 'm' + f'{tempo}' + 's', 1, preto)
     textpos = text.get_rect(centerx=730, centery=25)
@@ -35,73 +37,104 @@ def mostra_titulo_tela():
     tela.blit(text, textpos)
 
 
-def mostra_titulo_teste():
+def mostra_titulo_teste(texto):
     pygame.draw.rect(tela, laranja, (0, 100, 800, 700))  # exibe a tela com as dimensões especificadas
     font = pygame.font.Font(None, 30)
-    text = font.render('Teste!!!!', 1, preto)
+    text = font.render(texto, 1, preto)
     textpos = text.get_rect(centerx=tela.get_width() / 2, centery=tela.get_width() / 2)
     tela.blit(text, textpos)
 
 
 def desenha_abas():
     for i in range(4):
-        eixo_x = i * 200
-        pygame.draw.rect(tela, darkBlue, (eixo_x, 50, 200, 50), 0)
-        a = pygame.draw.rect(tela, darkBlue, (eixo_x, 50, 200, 50), 0)
-        pygame.draw.rect(tela, branco, (eixo_x, 50, 200, 50), 1)
-        mostra_titulo_abas(i, eixo_x)
+        x = i * 200
+        pygame.draw.rect(tela, darkBlue, (x, 50, 200, 50), 0)
+        a = pygame.draw.rect(tela, darkBlue, (x, 50, 200, 50), 0)
+        pygame.draw.rect(tela, branco, (x, 50, 200, 50), 1)
+        mostra_titulo_abas(i, x)
         lista.append(a)
 
 
-def mostra_titulo_abas(i, eixo_x):
+def mostra_titulo_abas(i, x):
     font = pygame.font.Font(None, 24)
     text = font.render(titulo_abas[i], 1, branco)
-    textpos = text.get_rect(centerx=eixo_x + 100, centery=75)
+    textpos = text.get_rect(centerx=x + 100, centery=75)
     tela.blit(text, textpos)
 
 
 def mostra_titulo_h2(tela, texto, x, y):
-    font = pygame.font.Font(None, 20)
+    font = pygame.font.Font('OpenSans-Regular.ttf', 15)
     text = font.render(texto, 1, preto)
-    textpos = text.get_rect(centerx=50, centery=150)
-    textpos.left = 50
+    textpos = text.get_rect(centerx=x + 100, centery=y+150)
+    textpos.left = x
     tela.blit(text, textpos)
 
 
-def desenha_tela_inicial():
-    desenha_abas()
-    
-    
+caminho_pasta = os.getcwd()
+arquivo = os.listdir(caminho_pasta)
+titulo_arquivo = ['Arquivo', 'Tamanho', 'Criado em', 'Última Modificação']
+
+
+def conteudo_aba0():
+    soma = 1
+    pygame.draw.rect(tela, cinza, (0, 0, 800, 800), 0)
+    mostra_titulo_h2(tela, f'{titulo_arquivo[0]}', x=25, y=5)
+    mostra_titulo_h2(tela, f'{titulo_arquivo[1]}', x=200, y=5)
+    mostra_titulo_h2(tela, f'{titulo_arquivo[2]}', x=400, y=5)
+    mostra_titulo_h2(tela, f'{titulo_arquivo[3]}', x=600, y=5)
+
+    for i in range(len(arquivo)):
+        status_arquivo = os.stat(f'{arquivo[i]}')
+        # data_criacao = time.localtime(status_arquivo.st_ctime)
+        # ultima_modificacao = time.localtime(status_arquivo.st_mtime)
+        mostra_titulo_h2(tela, f'{arquivo[i]}', x=25, y=soma * 30)
+        mostra_titulo_h2(tela, f'{status_arquivo.st_size/1024:.2f} KBytes', x=200, y=soma*30)
+        mostra_titulo_h2(tela, f'{time.strftime("%d/%m/%Y     %H:%M:%S", time.localtime(status_arquivo.st_ctime))}', x=400, y=soma * 30)
+        mostra_titulo_h2(tela, f'{time.strftime("%d/%m/%Y     %H:%M:%S", time.localtime(status_arquivo.st_mtime))}', x=600, y=soma * 30)
+        soma += 1
+        os.times()
 
 
 soma_rss = sum([i["RSS"] for i in lista_dicionarios])/1024/1024/1024
 soma_vms = sum([i["VMS"] for i in lista_dicionarios])/1024/1024/1024
 soma = 0
 soma_media = 0
+titulo_conteudo_aba1 = ['Processo', 'Nome', 'Tamanho vms', 'Tamanho rss', '% vms', '% rss']
 
-def formata_valores():
+
+def conteudo_aba1():
     soma_indices = 1
+    # mostra_tempo(tempo, minuto)
+    pygame.draw.rect(tela, cinza, (0, 0, 800, 800), 0)
+    mostra_titulo_h2(tela, titulo_conteudo_aba1[0], 50, -40)
+    mostra_titulo_h2(tela, titulo_conteudo_aba1[1], 150, -40)
+    mostra_titulo_h2(tela, titulo_conteudo_aba1[2], 320, -40)
+    mostra_titulo_h2(tela, titulo_conteudo_aba1[3], 470, -40)
+    mostra_titulo_h2(tela, titulo_conteudo_aba1[4], 600, -40)
+    mostra_titulo_h2(tela, titulo_conteudo_aba1[5], 700, -40)
+
     for p in psutil.process_iter():
         if p.status() == "running":
+
             vms = p.memory_info().vms/1024/1024
             rss = p.memory_info().rss/1024/1024
-            mostra_titulo_h2(tela, f'{p.pid}', 100, 160 + soma_indices * 22)
-            mostra_titulo_h2(tela, f'{p.name()}', 160, 160 + soma_indices * 22)
-            mostra_titulo_h2(tela, f'{round(vms, 2)}MB', 400, 160 + soma_indices * 22)
-            mostra_titulo_h2(tela, f'{round(rss, 2)}MB', 500, 160 + soma_indices * 22)
-            mostra_titulo_h2(tela, f'{round((vms/(soma_vms*1024)) * 100, 2)}%', 620, 160 + soma_indices * 22)
-            mostra_titulo_h2(tela, f'{round((rss/(soma_rss*1024)) * 100, 2)}%', 700, 160 + soma_indices * 22)
-            soma_indices = soma_indices + 1        
-
-
+            mostra_titulo_h2(tela, f'{p.pid}', 50, -30 + soma_indices * 22)
+            mostra_titulo_h2(tela, f'{p.name()}', 150, -30 + soma_indices * 22)
+            mostra_titulo_h2(tela, f'{round(vms, 2)} MB', 320, -30 + soma_indices * 22)
+            mostra_titulo_h2(tela, f'{round(rss, 2)} MB', 470, -30 + soma_indices * 22)
+            mostra_titulo_h2(tela, f'{round((vms/(soma_vms*1024)) * 100, 2)}%', 600, -30 + soma_indices * 22)
+            mostra_titulo_h2(tela, f'{round((rss/(soma_rss*1024)) * 100, 2)}%', 700, -30 + soma_indices * 22)
+            soma_indices += 1
 
 
 conta_clocks = 0
 conta_segundos = 0
 conta_minutos = 0
 lista = []
+aba1 = False
 
-terminou = False                                                        #variável para manter a tela aberta
+terminou = False
+#variável para manter a tela aberta
 while not terminou:
     # Checar os eventos do mouse aqui:
     for event in pygame.event.get():
@@ -110,31 +143,38 @@ while not terminou:
             for q in lista:
                 if q.collidepoint(pos):
                     if q == lista[0]:
-                        tela.fill(cinza)
+                        conteudo_aba0()
+                        aba1 = False
                         break
                     elif q == lista[1]:
-                        mostra_titulo_h2(f'{formata_valores()}', 150)
-                        print(lista_dicionarios)
-                        #tela.fill(cinza_escuro)
+                        conteudo_aba1()
+                        aba1 = True
                         break
                     elif q == lista[2]:
-                        mostra_titulo_teste()
+                        aba1 = False
+                        mostra_titulo_teste('Vai que cola?!')
                         break
                     elif q == lista[3]:
+                        aba1 = False
                         tela.fill(branco)
                         break
 
-        if event.type == pygame.QUIT:                                   #se clicar no X fecha a tela
-            terminou = True
+    if aba1 is True:
+        conteudo_aba1()
+
+    if event.type == pygame.QUIT:                                   #se clicar no X fecha a tela
+        terminou = True
 
     conta_clocks = conta_clocks + 1
+
+    # conteudo_aba1()
 
     if conta_clocks == 50:
         conta_segundos += 1
         conta_clocks = 0
 
     # desenha_tela_inicial()
-    desenha_abas()
+    # desenha_abas()
 
     # Mostra o tempo atualizado
     if conta_segundos == 60:
