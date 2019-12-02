100e7484afe1e184f99eee95a645dff98a5122ef
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2019-12-02 09:22:29 -0300

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -165,6 +165,44 @@ def conteudo_aba3():
         
         soma_indices += 1
 
+        
+def conteudo_aba3():
+    soma_indices = 1
+    pygame.draw.rect(tela, cinza, (0, 0, 1200, 800), 0)
+    import matplotlib
+    import matplotlib.pyplot as plt
+    matplotlib.use("Agg")
+    import matplotlib.backends.backend_agg as agg
+    mostra_titulo_h2(tela, "Gráfico de Processos PID x VMS", 500, -40)
+    processos_infos = []
+    for processos in psutil.process_iter():
+        processos_infos.append(
+            {"pid" :processos.pid,
+             "nome" : processos.name(),
+             "rss" : processos.memory_info().rss,
+             "vms" : processos.memory_info().vms})
+    names = [info["pid"] for info in processos_infos]
+    values = [info["vms"]/1024/1024 for info in processos_infos]
+    values_rss = [info["rss"]/1024/1024 for info in processos_infos]
+    
+    fig, axs = plt.subplots()
+    lines, = axs.plot(names, values)
+    plt.xlabel("pid")
+    plt.ylabel("vms")
+    plt.setp(lines, color = 'b', linewidth=1.0)
+    fig.suptitle('PID X VMS')
+
+    canvas = agg.FigureCanvasAgg(fig)
+    canvas.draw()
+    tamanho = canvas.get_width_height()
+    renderer = canvas.get_renderer()
+    raw_data = renderer.tostring_rgb()
+
+    superficie_grafico = pygame.image.fromstring(raw_data, tamanho, "RGB")
+    tela.blit(superficie_grafico, (0, 150))
+    
+    superficie_grafico = pygame.image.fromstring(raw_data, tamanho, "RGB")
+    tela.blit(superficie_grafico, (600, 150))
 
 def conteudo_aba4():
     mostra_titulo_h2(tela, "Gráfico de Processos PID x VMS", 500, -40)
